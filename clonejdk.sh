87ea29d942c95ebf7bb88caa98c5012361cea89b
VladDeveloper <71747467+Vlad-Games@users.noreply.github.com>
2025-11-20 09:41:14 +0200

    Update JDK clone source for arm and iOS builds
    Replaced the Corretto 8 repository with a different source for cloning.

--- clonejdk.sh
+++ clonejdk.sh
@@ -3,11 +3,9 @@ set -e
 if [[ "$TARGET_JDK" == "arm" ]]; then
 git clone --depth 1 https://github.com/openjdk/aarch32-port-jdk8u openjdk
 elif [[ "$BUILD_IOS" == "1" ]]; then
-git clone --depth 1 https://github.com/corretto/corretto-8 openjdk
+#git clone --depth 1 https://github.com/corretto/corretto-8 openjdk
 # Fix invalid ISO-8859-1 byte in Encodings.properties (macOS awk fix)
-cd openjdk
-git apply ../fix-encodings.patch
-cd ..
+git clone --depth 1 https://github.com/Vlad-Games/corretto-8 openjdk
 else
 # Use aarch32 repo because it also has aarch64
 
