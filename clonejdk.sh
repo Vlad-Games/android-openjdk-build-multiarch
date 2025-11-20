4874ea427b1cf76960c473a95d4648932eccc7de
VladDeveloper <71747467+Vlad-Games@users.noreply.github.com>
2025-11-20 08:01:29 +0200

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -4,6 +4,10 @@ if [[ "$TARGET_JDK" == "arm" ]]; then
 git clone --depth 1 https://github.com/openjdk/aarch32-port-jdk8u openjdk
 elif [[ "$BUILD_IOS" == "1" ]]; then
 git clone --depth 1 https://github.com/corretto/corretto-8 openjdk
+# Fix UTF-8 corruption in Corretto 8
+find openjdk/jaxp -type f -name '*.properties' -exec sh -c '
+    iconv -f utf-8 -t utf-8 -c "$1" > "$1.fixed" && mv "$1.fixed" "$1"
+' _ {} \;
 else
 # Use aarch32 repo because it also has aarch64
 
