85d46d483e67b6b11c0229491a8d610ea4197e52
VladDeveloper <71747467+Vlad-Games@users.noreply.github.com>
2025-11-20 08:16:52 +0200

    Modify UTF-8 fix for Encodings.properties
    Update sed command to fix UTF-8 corruption in Encodings.properties.

--- clonejdk.sh
+++ clonejdk.sh
@@ -4,8 +4,8 @@ if [[ "$TARGET_JDK" == "arm" ]]; then
 git clone --depth 1 https://github.com/openjdk/aarch32-port-jdk8u openjdk
 elif [[ "$BUILD_IOS" == "1" ]]; then
 git clone --depth 1 https://github.com/corretto/corretto-8 openjdk
-# Fix UTF-8 corruption in Corretto 8
-sed -i.bak 's/�vard/Edvard/' openjdk/jaxp/src/com/sun/org/apache/xml/internal/serializer/Encodings.properties || true
+LC_ALL=C sed -i.bak 's/.\{1\}vard Wigtil)/Edvard Wigtil)/' \
+    openjdk/jaxp/src/com/sun/org/apache/xml/internal/serializer/Encodings.properties
 else
 # Use aarch32 repo because it also has aarch64
 
