b178e53d3fb4e8c920f6b9076b507787324888c4
VladDeveloper <71747467+Vlad-Games@users.noreply.github.com>
2025-11-20 09:12:59 +0200

    Fix invalid byte in Encodings.properties for macOS
    Updated the sed command to fix invalid ISO-8859-1 byte in Encodings.properties for macOS compatibility.

--- clonejdk.sh
+++ clonejdk.sh
@@ -4,8 +4,8 @@ if [[ "$TARGET_JDK" == "arm" ]]; then
 git clone --depth 1 https://github.com/openjdk/aarch32-port-jdk8u openjdk
 elif [[ "$BUILD_IOS" == "1" ]]; then
 git clone --depth 1 https://github.com/corretto/corretto-8 openjdk
-# Fix invalid UTF-8 in Encodings.properties (macOS awk crashes otherwise)
-sed -i.bak $'s/H\xEF\xBF\xBDvard/Havard/' openjdk/jaxp/src/com/sun/org/apache/xml/internal/serializer/Encodings.properties || true
+# Fix invalid ISO-8859-1 byte in Encodings.properties (macOS awk fix)
+LC_ALL=C sed -i.bak $'s/H\xa5vard/Havard/' openjdk/jaxp/src/com/sun/org/apache/xml/internal/serializer/Encodings.properties
 else
 # Use aarch32 repo because it also has aarch64
 
