a07710a466779b098b672899d0fbb2bcc62c49a1
VladDeveloper <71747467+Vlad-Games@users.noreply.github.com>
2025-11-20 09:27:37 +0200

    Clone OpenJDK and apply encoding fix patch
    Apply patch to fix invalid byte in Encodings.properties after cloning.

--- clonejdk.sh
+++ clonejdk.sh
@@ -5,7 +5,9 @@ git clone --depth 1 https://github.com/openjdk/aarch32-port-jdk8u openjdk
 elif [[ "$BUILD_IOS" == "1" ]]; then
 git clone --depth 1 https://github.com/corretto/corretto-8 openjdk
 # Fix invalid ISO-8859-1 byte in Encodings.properties (macOS awk fix)
-LC_ALL=C sed -i.bak $'s/H\xa5vard/Havard/' openjdk/jaxp/src/com/sun/org/apache/xml/internal/serializer/Encodings.properties
+cd openjdk
+git apply ../fix-encodings.patch
+cd ..
 else
 # Use aarch32 repo because it also has aarch64
 
