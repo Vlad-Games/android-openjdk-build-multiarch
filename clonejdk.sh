25bcb0dd1c71249fc1b72b6ae35a1f262601b052
VladDeveloper <71747467+Vlad-Games@users.noreply.github.com>
2025-11-20 08:12:40 +0200

    Update UTF-8 fix for Encodings.properties
    Replaced UTF-8 corruption fix with sed command to correct a specific encoding issue in the properties file.

--- clonejdk.sh
+++ clonejdk.sh
@@ -5,9 +5,7 @@ git clone --depth 1 https://github.com/openjdk/aarch32-port-jdk8u openjdk
 elif [[ "$BUILD_IOS" == "1" ]]; then
 git clone --depth 1 https://github.com/corretto/corretto-8 openjdk
 # Fix UTF-8 corruption in Corretto 8
-find openjdk/jaxp -type f -name '*.properties' -exec sh -c '
-    iconv -f utf-8 -t utf-8 -c "$1" > "$1.fixed" && mv "$1.fixed" "$1"
-' _ {} \;
+sed -i.bak 's/�vard/Edvard/' openjdk/jaxp/src/com/sun/org/apache/xml/internal/serializer/Encodings.properties || true
 else
 # Use aarch32 repo because it also has aarch64
 
