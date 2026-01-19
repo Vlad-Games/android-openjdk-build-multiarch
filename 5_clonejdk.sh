b48445b56fbfeb07bd463f85c7d63766f3b0466e
VladDeveloper <71747467+Vlad-Games@users.noreply.github.com>
2026-01-20 01:02:31 +0200

    Modify JDK 17 clone and checkout process
    Updated the cloning process for JDK 17 to include checkout of a specific version.
    
    Signed-off-by: VladDeveloper <71747467+Vlad-Games@users.noreply.github.com>

--- 5_clonejdk.sh
+++ 5_clonejdk.sh
@@ -4,5 +4,9 @@ set -e
 if [[ $TARGET_VERSION -eq 21 ]]; then
     git clone --branch jdk21.0.1 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
 else
-    git clone --depth 1 https://github.com/openjdk/jdk17u openjdk-17
+    # git clone --depth 1 https://github.com/openjdk/jdk17u openjdk-17
+    git clone https://github.com/openjdk/jdk17u.git openjdk-17
+    cd openjdk-17
+    git checkout jdk-17.0.16+2
+    cd ..
 fi
